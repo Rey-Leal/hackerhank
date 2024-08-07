c1025571d97e05a5bdb80879b31b0d02e407babb
ReyLeal <reinaldoleal2@gmail.com>
2024-08-07 15:11:07 -0300

    Sync JSON

---
+++ HackerRank/99Testes.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+
+////Testes em geral
+//class Testes
+//{
+//    //class
+//}
+
+//class Solution
+//{
+//    public static void Main(string[] args)
+//    {
+//        try
+//        {
+//            decimal resultado = 1582.40M;
+//            double numero = 0.5636;
+//            int inteiro = 4567;
+//            DateTime data = DateTime.Now;
+
+//            Console.WriteLine($"{resultado:C}");
+//            Console.WriteLine($"{resultado:N4}");
+//            Console.WriteLine($"{numero.ToString("P")}");
+//            Console.WriteLine($"{inteiro.ToString("##-##")}");
+//            Console.WriteLine($"{data.ToString("dd/MM/yyyy HH:mm")}");
+
+//        }
+//        catch (Exception erro)
+//        {
+//            Console.WriteLine(erro.Message.ToString());
+//            Console.ReadLine();
+//        }
+//    }
+//}
