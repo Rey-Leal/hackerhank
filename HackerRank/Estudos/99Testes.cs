c542a414df57b56dcc24cd9c963b88f66db18f83
ReyLeal <reinaldoleal2@gmail.com>
2024-08-08 14:23:29 -0300

    Sync

--- HackerRank/Estudos/99Testes.cs
+++ HackerRank/Estudos/99Testes.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using Newtonsoft.Json;
 
-public class Pessoa
-{
-    public override string ToString()
-    {
-        return "Alterei esse metodo interno kkkk!";
-    }
+//public class Pessoa
+//{
+//    public override string ToString()
+//    {
+//        return "Alterei esse metodo interno!";
+//    }
 
-}
+//}
 
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        try
-        {
-            Pessoa pessoa = new Pessoa();
-            int numero = 3242342;
-            Console.WriteLine(pessoa.ToString());
-            Console.WriteLine(numero.ToString());
-        }
-        catch (Exception erro)
-        {
-            Console.WriteLine(erro.Message.ToString());
-            Console.ReadLine();
-        }
-    }
-}
+//class Solution
+//{
+//    public static void Main(string[] args)
+//    {
+//        try
+//        {
+//            Pessoa pessoa = new Pessoa();
+//            int numero = 3242342;
+//            Console.WriteLine(pessoa.ToString());
+//            Console.WriteLine(numero.ToString());
+//        }
+//        catch (Exception erro)
+//        {
+//            Console.WriteLine(erro.Message.ToString());
+//            Console.ReadLine();
+//        }
+//    }
+//}
