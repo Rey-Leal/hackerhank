6204a1063c07f114b9fd76bbe4ecff2d30bdcc45
ReyLeal <reinaldoleal2@gmail.com>
2024-08-08 11:22:57 -0300

    Organize

---
+++ HackerRank/Estudos/99Testes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public class Pessoa
+{
+    public override string ToString()
+    {
+        return "Alterei esse metodo interno kkkk!";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Pessoa pessoa = new Pessoa();
+            int numero = 3242342;
+            Console.WriteLine(pessoa.ToString());
+            Console.WriteLine(numero.ToString());
+        }
+        catch (Exception erro)
+        {
+            Console.WriteLine(erro.Message.ToString());
+            Console.ReadLine();
+        }
+    }
+}
